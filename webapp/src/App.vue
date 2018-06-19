4144711402aef508ba08788c280d603b002656fe
zlig <gdevillard@gmail.com>
2018-06-19 22:22:12 +0100

    Adding bootstrap-vue components

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <b-nav justified tabs>
+      <b-nav-item active>Hello</b-nav-item>
+      <b-nav-item>Bye</b-nav-item>
+      <b-nav-item>Not Found</b-nav-item>
+    </b-nav>
   </div>
 </template>
 
