4e32574a6b9ba918dec92159e0e8bd00e59583e1
zlig <gdevillard@gmail.com>
2018-06-19 22:33:47 +0100

    Linter issues

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -7,9 +7,9 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
-
 import { Nav } from 'bootstrap-vue/es/components'
+
+Vue.use(BootstrapVue)
 Vue.use(Nav)
 
 Vue.config.productionTip = false
