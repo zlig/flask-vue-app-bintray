a27d7b31515ca056f14611cb123d111957edab23
zlig <gdevillard@gmail.com>
2018-06-20 15:55:49 +0100

    Adds Lazy loading of views

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,34 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
+/*import HelloWorld from '@/components/HelloWorld'
 import HiName from '@/components/HiName'
 import GoodBye from '@/components/GoodBye'
-import NotFound from '@/components/NotFound'
+import NotFound from '@/components/NotFound'*/
 
 Vue.use(Router)
+
+function load(view) {
+  return () => import('@/components/${view}.vue')
+}
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: load('HelloWorld')
     },
     {
       path: '/hi',
       name: 'HiName',
-      component: HiName
+      component: load('HiName')
     },
     {
       path: '/bye',
       name: 'GoobBye',
-      component: GoodBye
+      component: load('GoodBye')
     },
     {
       path: '/404',
       name: '404',
-      component: NotFound
+      component: load('NotFound')
     },
     {
       path: '*',
